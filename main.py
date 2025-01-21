04c837c7622bbdc201621175242f27d86c20d1b0
Kartikey Chauhan <me@cskartikey.dev>
2025-01-21 21:59:37 +0530

    fix: comment unused lines

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from config import app, PORT
 from commands.sonar import handle_sonar
 from actions.load_more import load_more
 from actions.prev_page import prev_page
-from actions.alt_pagination import handle_alt_page
+# from actions.alt_pagination import handle_alt_page
 from actions.sonar_actions import handle_sonar_action
 from logs.data_fetcher import fetch_historical_data, fetch_incremental_data
 from utils.elastic_search import create_index
@@ -13,11 +13,11 @@ from utils.elastic_search import create_index
 app.command("/sonar")(handle_sonar)
 
 app.action("search_action")(handle_sonar_action)
-app.action("find_alts_action")(handle_sonar_action)
+# app.action("find_alts_action")(handle_sonar_action)
 app.action("load_more")(load_more)
 app.action("prev_page")(prev_page)
-app.action("next_alt_page")(handle_alt_page)
-app.action("prev_alt_page")(handle_alt_page)
+# app.action("next_alt_page")(handle_alt_page)
+# app.action("prev_alt_page")(handle_alt_page)
 
 
 async def data_fetcher():
